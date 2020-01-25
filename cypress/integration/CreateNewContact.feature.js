f84ddbc596a84efd87e0d311df53d1dd4e5ce909
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-01-25 12:38:38 +0100

    added more tests

--- cypress/integration/CreateNewContact.feature.js
+++ cypress/integration/CreateNewContact.feature.js
@@ -8,21 +8,31 @@ I would like to be able to create a contact for them in my address book
 // userCanCreateAContact.feature.js
 
 
-describe('user can access the site', () => {
+describe('1. User can access the site', () => {
 	it('test', () => {
         cy.visit('http://localhost:3001') 
 	})
 })
 
-describe('user can create a contact', () => {
-	it('test', () => {
-        cy.visit('http://localhost:3001')         
+describe('2. User can create a contact', () => {
+	it('test', () => {      
+        cy.visit('http://localhost:3001') 
 		cy.get('#name').type('Robin')
 		cy.get('#email').type('robin@abacuz.se')
 		cy.get('#phone').type('0700 101010')
 		cy.get('#company').type('Craft Academy')
 		cy.get('#notes').type('Awesome coder')
         cy.get('#twitter').type('@abacuz08')
-        cy.get('#add-contact').click()
+        cy.get('#submit').click()
 	})
+})
+
+it.skip('3. Displays a name of the new contact', () => {
+    cy.visit('http://localhost:3001') 
+    cy.get('#contact-list').should('contain', 'Thomas')
+})
+
+it.skip('4. Displays the phone number of the new contact', () => {
+    cy.visit('http://localhost:3001') 
+    cy.get('#contact-list').should('contain', '0700 101010')
 })
