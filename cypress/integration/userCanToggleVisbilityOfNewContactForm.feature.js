4ff8ca3d4e757fe4c68c8f358c4ab9445a70bcbb
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-01-27 09:09:32 +0100

    updated the styling a little

--- cypress/integration/userCanToggleVisbilityOfNewContactForm.feature.js
+++ cypress/integration/userCanToggleVisbilityOfNewContactForm.feature.js
@@ -5,10 +5,10 @@ describe('user can toggle visibility of the form', () => {
 	})
 	
 	it('by clicking the "Add Contact" button', () => {
-	cy.get('#new-contact-form').should('not.be.visible')
-	cy.get('#add-contact').click()
-	cy.get('#new-contact-form').should('be.visible')
-	cy.get('#add-contact').click()
-	cy.get('#new-contact-form').should('not.be.visible')
+        cy.get('#new-contact-form').should('not.be.visible')
+        cy.get('#add-contact').click()
+        cy.get('#new-contact-form').should('be.visible')
+        cy.get('#add-contact').click()
+        cy.get('#new-contact-form').should('not.be.visible')
 	})
 })
