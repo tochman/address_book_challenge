4bb699d80a4786dce0cb57e1d0083f5b0652f63d
Thomas <thomas@craftacademy.se>
2020-01-29 13:12:20 +0100

    code formatting

--- cypress/integration/DeleteExistingContact.feature.js
+++ cypress/integration/DeleteExistingContact.feature.js
@@ -4,21 +4,21 @@ In order to keep my contact list up to date
 I would like to be able to delete contacts
 */
 
-describe('clicking the "Delete"', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3001')
-        cy.get('#add-contact').click()
-        cy.get('#name').type('Robin')
-        cy.get('#email').type('robin@abacuz.se')
-        cy.get('#phone').type('0700101010')
-        cy.get('#company').type('Craft Academy')
-        cy.get('#notes').type('Awesome coder')
-        cy.get('#twitter').type('Abacuz08')
-        cy.get('#submit').click()
-    })
+describe('clicking the "Delete" button', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3001')
+    cy.get('#add-contact').click()
+    cy.get('#name').type('Robin')
+    cy.get('#email').type('robin@abacuz.se')
+    cy.get('#phone').type('0700101010')
+    cy.get('#company').type('Craft Academy')
+    cy.get('#notes').type('Awesome coder')
+    cy.get('#twitter').type('Abacuz08')
+    cy.get('#submit').click()
+  })
 
-    it(' removes the contact', () => {
-        cy.get('#delete-button').click()
-        cy.contains('robin@abacuz.se').should('not.exist')
-    })
+  it('removes the contact', () => {
+    cy.get('#delete-button').click()
+    cy.contains('robin@abacuz.se').should('not.exist')
+  })
 });
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,17 +1,17 @@
 const storage = window.localStorage
 
 const renderContacts = () => {
-	const contacts = JSON.parse(storage.getItem('contacts'))
+  const contacts = JSON.parse(storage.getItem('contacts'))
 
-	let div = document.getElementById('contact-list')
-	if (contacts) {
-		div.innerHTML = ''
-		i = 0
-		contacts.forEach(contact => {
-			var newDiv = document.createElement("div");
-			newDiv.classList.add("contact-card");
-			newDiv.innerHTML =
-				`
+  let div = document.getElementById('contact-list')
+  if (contacts) {
+    div.innerHTML = ''
+    i = 0
+    contacts.forEach(contact => {
+      var newDiv = document.createElement("div");
+      newDiv.classList.add("contact-card");
+      newDiv.innerHTML =
+        `
 				<div class="contact-name">${contact.name}</div>
 				<div class="contact-email">${contact.email}</div>
 				<div class="contact-phone">${contact.phone}</div>
@@ -21,96 +21,96 @@ const renderContacts = () => {
 				<input type="button" value="Update" id="update-button" onclick="updateContact(${i})"/>
 				<input type="button" value="Delete" id="delete-button" onclick="removeContact(${i})"/>
 				`
-			div.appendChild(newDiv)
-			i++;
-		})
-	} else {
-		div.innerHTML = '<p>You have no contacts in your address book</p>'
-	}
+      div.appendChild(newDiv)
+      i++;
+    })
+  } else {
+    div.innerHTML = '<p>You have no contacts in your address book</p>'
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	renderContacts()
-	const contactForm = document.getElementById('new-contact-form')
-	const toggleFormVisibilityButton = document.getElementById('add-contact')
-	contactForm.style.display = 'none'
+  renderContacts()
+  const contactForm = document.getElementById('new-contact-form')
+  const toggleFormVisibilityButton = document.getElementById('add-contact')
+  contactForm.style.display = 'none'
 
-	toggleFormVisibilityButton.addEventListener('click', () => {
-		if (contactForm.style.display === '') {
-			contactForm.style.display = 'none'
-		} else {
-			contactForm.style.display = ''
-		}
-	})
+  toggleFormVisibilityButton.addEventListener('click', () => {
+    if (contactForm.style.display === '') {
+      contactForm.style.display = 'none'
+    } else {
+      contactForm.style.display = ''
+    }
+  })
 
-	contactForm.addEventListener('submit', event => {
-		event.preventDefault()
+  contactForm.addEventListener('submit', event => {
+    event.preventDefault()
 
-		// 1. Read all the input fields and get their values
-		const { name, email, phone, company, notes, twitter } = contactForm.elements
+    // 1. Read all the input fields and get their values
+    const { name, email, phone, company, notes, twitter } = contactForm.elements
 
-		const contact = {
-			name: name.value,
-			email: email.value,
-			phone: phone.value,
-			company: company.value,
-			notes: notes.value,
-			twitter: twitter.value,
-		}
+    const contact = {
+      name: name.value,
+      email: email.value,
+      phone: phone.value,
+      company: company.value,
+      notes: notes.value,
+      twitter: twitter.value,
+    }
 
-		let contacts = JSON.parse(storage.getItem('contacts')) || []
-		contacts.push(contact)
+    let contacts = JSON.parse(storage.getItem('contacts')) || []
+    contacts.push(contact)
 
-		// 2. Save them to our storage
-		storage.setItem('contacts', JSON.stringify(contacts))
-		renderContacts()
-		contactForm.reset()
-	})
+    // 2. Save them to our storage
+    storage.setItem('contacts', JSON.stringify(contacts))
+    renderContacts()
+    contactForm.reset()
+  })
 })
 
 function updateContact(toBeRemoved) {
 
-	displayUpdateFields(toBeRemoved)
+  displayUpdateFields(toBeRemoved)
 
-	const updateContactForm = document.getElementById('update-contact-form')
+  const updateContactForm = document.getElementById('update-contact-form')
 
-	contactForm.addEventListener('submit', event => {
-		event.preventDefault()
+  contactForm.addEventListener('submit', event => {
+    event.preventDefault()
 
-		// 1. Read all the input fields and get their values
-		const { name, email, phone, company, notes, twitter } = contactForm.elements
+    // 1. Read all the input fields and get their values
+    const { name, email, phone, company, notes, twitter } = contactForm.elements
 
-		const contact = {
-			name: name.value,
-			email: email.value,
-			phone: phone.value,
-			company: company.value,
-			notes: notes.value,
-			twitter: twitter.value,
-		}
+    const contact = {
+      name: name.value,
+      email: email.value,
+      phone: phone.value,
+      company: company.value,
+      notes: notes.value,
+      twitter: twitter.value,
+    }
 
-		storage.setItem('contacts', JSON.stringify(contacts))
-		renderContacts()
-		contactForm.reset()
-	})
+    storage.setItem('contacts', JSON.stringify(contacts))
+    renderContacts()
+    contactForm.reset()
+  })
 
-	let newContacts = JSON.parse(storage.getItem('contacts'))
-	debugger
-	newContacts.splice(toBeRemoved, 1);
+  let newContacts = JSON.parse(storage.getItem('contacts'))
+  debugger
+  newContacts.splice(toBeRemoved, 1);
 }
 
 function removeContact(toBeRemoved) {
-	let newContacts = JSON.parse(storage.getItem('contacts'))
-	newContacts.splice(toBeRemoved, 1);
-	storage.clear()
-	storage.setItem('contacts', JSON.stringify(newContacts))
-	renderContacts()
+  let newContacts = JSON.parse(storage.getItem('contacts'))
+  newContacts.splice(toBeRemoved, 1);
+  storage.clear()
+  storage.setItem('contacts', JSON.stringify(newContacts))
+  renderContacts()
 }
 
 
 function removeAllContact() {
-	storage.clear()
-	renderContacts()
+  storage.clear()
+  renderContacts()
 }
 
 function displayUpdateFields(id) {
