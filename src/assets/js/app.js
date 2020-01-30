f87e5da3b1afef3fc0ddd6ad06b72c27b0ad6e3c
Thomas <thomas@craftacademy.se>
2020-01-30 15:20:42 +0100

    WIP form vissibility confusion...

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,56 +2,57 @@ const storage = window.localStorage
 let contactToUpdate = null
 
 const removeContact = (toBeRemoved) => {
-  let newContacts = JSON.parse(storage.getItem('contacts'))
-  newContacts.splice(toBeRemoved, 1);
-  storage.clear()
-  storage.setItem('contacts', JSON.stringify(newContacts))
-  renderContacts()
+	let newContacts = JSON.parse(storage.getItem('contacts'))
+	newContacts.splice(toBeRemoved, 1);
+	storage.clear()
+	storage.setItem('contacts', JSON.stringify(newContacts))
+	renderContacts()
 }
 
 
 const removeAllContact = () => {
-  storage.clear()
-  renderContacts()
+	storage.clear()
+	renderContacts()
 }
 
 const displayUpdateFields = id => {
-  let contacts = JSON.parse(localStorage.getItem('contacts'))
-  let contactToUpdate = contacts[id]
-  let form = document.getElementById('new-contact-form')
-  let indexField = document.createElement('input')
-  indexField.type = 'hidden'
-  indexField.name = 'index'
-  indexField.value = id
-  form.appendChild(indexField)
-  form.elements.type.value = 'updateContact'
-  form.elements.name.value = contactToUpdate.name
-  form.elements.email.value = contactToUpdate.email
-  form.elements.phone.value = contactToUpdate.phone
-  form.elements.company.value = contactToUpdate.company
-  form.elements.notes.value = contactToUpdate.notes
-  form.elements.twitter.value = contactToUpdate.twitter
-  form.elements.submit.value = "Update contact"
+	let contacts = JSON.parse(localStorage.getItem('contacts'))
+	let contactToUpdate = contacts[id]
+	let form = document.getElementById('new-contact-form')
+	let indexField = document.createElement('input')
+	indexField.type = 'hidden'
+	indexField.name = 'index'
+	indexField.value = id
+	form.appendChild(indexField)
+	form.elements.type.value = 'updateContact'
+	form.elements.name.value = contactToUpdate.name
+	form.elements.email.value = contactToUpdate.email
+	form.elements.phone.value = contactToUpdate.phone
+	form.elements.company.value = contactToUpdate.company
+	form.elements.notes.value = contactToUpdate.notes
+	form.elements.twitter.value = contactToUpdate.twitter
+	form.elements.submit.value = "Update contact"
 }
 
 const updateContact = (toBeRemoved) => {
-  displayUpdateFields(toBeRemoved)
-  let newContacts = JSON.parse(storage.getItem('contacts'))
-  newContacts.splice(toBeRemoved, 1);
+  toggleFormVisibility(contactForm)
+	displayUpdateFields(toBeRemoved)
+	let newContacts = JSON.parse(storage.getItem('contacts'))
+	newContacts.splice(toBeRemoved, 1);
 }
 
 
 const renderContacts = () => {
-  const contacts = JSON.parse(storage.getItem('contacts'))
-  let div = document.getElementById('contact-list')
-  if (contacts) {
-    div.innerHTML = ''
-    i = 0
-    contacts.forEach(contact => {
-      var newDiv = document.createElement("div");
-      newDiv.classList.add("contact-card");
-      newDiv.innerHTML =
-        `
+	const contacts = JSON.parse(storage.getItem('contacts'))
+	let div = document.getElementById('contact-list')
+	if (contacts) {
+		div.innerHTML = ''
+		i = 0
+		contacts.forEach(contact => {
+			var newDiv = document.createElement("div");
+			newDiv.classList.add("contact-card");
+			newDiv.innerHTML =
+				`
 				<div class="contact-name">${contact.name}</div>
 				<div class="contact-email">${contact.email}</div>
 				<div class="contact-phone">${contact.phone}</div>
@@ -61,47 +62,51 @@ const renderContacts = () => {
 				<input type="button" value="Update" class="update-button" onclick="updateContact(${i})"/>
 				<input type="button" value="Delete" class="delete-button" onclick="removeContact(${i})"/>
 				`
-      div.appendChild(newDiv)
-      i++;
-    })
-  } else {
-    div.innerHTML = '<p>You have no contacts in your address book</p>'
-  }
+			div.appendChild(newDiv)
+			i++;
+		})
+	} else {
+		div.innerHTML = '<p>You have no contacts in your address book</p>'
+	}
+}
+
+const toggleFormVisibility = (contactForm) => {
+	if (contactForm.style.display === '') {
+		contactForm.style.display = 'none'
+	} else {
+		contactForm.style.display = ''
+	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  renderContacts()
+	renderContacts()
   const contactForm = document.getElementById('new-contact-form')
-  const toggleFormVisibilityButton = document.getElementById('add-contact')
-  contactForm.style.display = 'none'
+	const toggleFormVisibilityButton = document.getElementById('add-contact')
+	contactForm.style.display = 'none'
 
-  toggleFormVisibilityButton.addEventListener('click', () => {
-    if (contactForm.style.display === '') {
-      contactForm.style.display = 'none'
-    } else {
-      contactForm.style.display = ''
-    }
-  })
+	toggleFormVisibilityButton.addEventListener('click', toggleFormVisibility(contactForm))
 
-  contactForm.addEventListener('submit', event => {
-    event.preventDefault()
-    const { name, email, phone, company, notes, twitter } = contactForm.elements
-    const contact = {
-      name: name.value,
-      email: email.value,
-      phone: phone.value,
-      company: company.value,
-      notes: notes.value,
-      twitter: twitter.value,
-    }
-    let contacts = JSON.parse(storage.getItem('contacts')) || []
-    if (contactForm.elements.type.value === 'newContact') {
-      contacts.push(contact)
-    } else {
-      contacts[parseInt(contactForm.elements.index.value)] = contact
-    }
-    storage.setItem('contacts', JSON.stringify(contacts))
-    renderContacts()
-    contactForm.reset()
-  })
+	contactForm.addEventListener('submit', event => {
+		event.preventDefault()
+		const { name, email, phone, company, notes, twitter } = contactForm.elements
+		const contact = {
+			name: name.value,
+			email: email.value,
+			phone: phone.value,
+			company: company.value,
+			notes: notes.value,
+			twitter: twitter.value,
+		}
+		let contacts = JSON.parse(storage.getItem('contacts')) || []
+		if (contactForm.elements.type.value === 'newContact') {
+			contacts.push(contact)
+		} else {
+			contacts[parseInt(contactForm.elements.index.value)] = contact
+			contactForm.elements.submit.value = "Save contact"
+			toggleFormVisibility(contactForm)
+		}
+		storage.setItem('contacts', JSON.stringify(contacts))
+		renderContacts()
+		contactForm.reset()
+	})
 })
